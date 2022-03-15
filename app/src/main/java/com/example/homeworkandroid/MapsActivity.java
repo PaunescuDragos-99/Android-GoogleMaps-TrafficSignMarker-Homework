d34149e36bb89de6e0c4b4ec74fa344577c54630
PaunescuDragos-99 <dragospaunescu19@gmail.com>
2022-03-15 21:15:10 +0200

    Editare nume marker la apasarea lui.

--- app/src/main/java/com/example/homeworkandroid/MapsActivity.java
+++ app/src/main/java/com/example/homeworkandroid/MapsActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -17,6 +18,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.homeworkandroid.databinding.ActivityMapsBinding;
 
@@ -26,6 +28,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private ActivityMapsBinding binding;
     private static final String LOG_TAG =
             MapsActivity.class.getSimpleName();
+    private Marker markerSef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,13 +74,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions()
+        markerSef = mMap.addMarker(new MarkerOptions()
                 .position(sydney)
                 .title("Marker in Sydney")
                 .draggable(true));
+        markerSef.setTag(0);
 
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        mMap.setOnMarkerClickListener(this::onMarkerClick);
+
     }
 
     public void NewMarkerActivity(View view) {
@@ -92,7 +99,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .position(australia)
                 .title("Marker in Sydney")
                 .draggable(true));
+
     }
 
+    public boolean onMarkerClick(final Marker marker) {
 
+        // Retrieve the data from the marker.
+        Integer clickCount = (Integer) marker.getTag();
+
+        // Check if a click count was set, then display the click count.
+        if (clickCount != null) {
+            clickCount = clickCount + 1;
+            marker.setTag(clickCount);
+            marker.setTitle("America");
+            Toast.makeText(this,
+                    marker.getTitle() +
+                            " has been clicked " + clickCount + " times.",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        // Return false to indicate that we have not consumed the event and that we wish
+        // for the default behavior to occur (which is for the camera to move such that the
+        // marker is centered and for the marker's info window to open, if it has one).
+        return false;
+    }
 }
